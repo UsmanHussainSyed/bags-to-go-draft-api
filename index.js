9baa77cc2a3cfbe4c1503e004d2e8d4d24fecbf8
ElDorado92 <74913149+ElDorado92@users.noreply.github.com>
2023-06-20 22:56:56 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express();
+
+
+app.post('/createdraft',async (req,res)=>{
+  try{
+    console.log('started')
+    let products = req.body?.products
+
+    products = [...products,
+       {title:"Shipping Insurance",
+        quantity:1,
+         originalUnitPrice:5,
+        }]
+        const shop = 'bagstogogo.myshopify.com';
+        const accessToken = process.env.TOKEN;
+const url = `https://${shop}/admin/api/2023-04/graphql.json`;
+const headers = {
+  'Content-Type': 'application/json',
+  'X-Shopify-Access-Token': accessToken,
+};
+console.log(products)
+const body = {
+  query: `
+  mutation CreateDraftOrder($lineItems: [DraftOrderLineItemInput!]!) {
+    draftOrderCreate(input: {
+      lineItems: $lineItems
+    }) {
+      draftOrder {
+        id
+        order {
+          id
+        }
+        invoiceUrl
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+  `,
+  variables:{
+    lineItems:products
+  }
+};
+
+const data =await fetch(url,{
+
+  method:'POST',
+    body:JSON.stringify(body),
+    headers:headers
+})
+res.json(await data.json())
+}
+catch(err){
+  console.log(err)
+    res.json(err)
+}
+
+})
+
+app.listen(3000, ()=>{
+    console.log('server is running on 3000')
+})
+
+Paste your content here to scan...
+SCAN
